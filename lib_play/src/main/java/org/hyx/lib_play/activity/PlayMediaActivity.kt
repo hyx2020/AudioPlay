fbe8f333d8c5ac2fc4d2c79f019ea9d09bc02581
hyx <little_home_hyx@163.com>
2021-08-13 10:58:04 +0800

    添加navigation导航

---
+++ lib_play/src/main/java/org/hyx/lib_play/activity/PlayMediaActivity.kt
@@ -0,0 +1,12 @@
+package org.hyx.lib_play.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.hyx.lib_play.R
+
+class PlayMediaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play_media)
+    }
+}
